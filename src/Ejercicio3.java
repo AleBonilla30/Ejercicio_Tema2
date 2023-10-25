93284fe57176dcaa9147e62a06d19eca9d0cb568
Alejandra Bonilla <148543725+AleBonilla30@users.noreply.github.com>
2023-10-25 18:00:27 +0200

    Update Ejercicio3.java

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -4,18 +4,20 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner Variable1= new Scanner(System.in);
         System.out.println("La variable 1 es: ");
-       int Var1= Integer.parseInt(Variable1.nextLine());
+       int Var1= Variable1.nextint();
 
         Scanner Variable2= new Scanner(System.in);
         System.out.println("La variable dos es: ");
 
-        int Var2= Integer.parseInt(Variable2.nextLine());
+        int Var2=Variable2.nextint();
 
         System.out.println("La suma de las dos es: "+(Var1+Var2));
 
         System.out.println("La resta de las dos es: "+(Var1-Var2));
 
         System.out.println("La multiplicacion de las dos es: "+(Var1*Var2));
+
+        System.out.println("la division entera de las dos es: "+(Var1/Var2));
 
         System.out.println("La division de las dos es: "+(Var1%Var2));
 
